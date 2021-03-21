39598a3405b2cd7582984e76407a97ab13af0e5c
note <note16651@hotmail.co.th>
2021-03-21 16:19:36 +0700

    [Change] Snake

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/main.scss';
+// Import initial FontAwesome Styles: https://github.com/FortAwesome/react-fontawesome/issues/134#issuecomment-476276516
+import '@fortawesome/fontawesome-svg-core/styles.css'; 
 
-function MyApp({ Component, pageProps }) {
+// Import FontAwesome Icons
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faStar, faArrowUp, faArrowRight, faArrowDown, faArrowLeft, faTrophy } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faStar, faArrowUp, faArrowRight, faArrowDown, faArrowLeft, faTrophy, faGithub)
+config.autoAddCss = false
+
+export default function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
